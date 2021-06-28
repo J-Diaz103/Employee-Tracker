0848d1c92ae9763f8810b091b0a8f0377373f921
J Diaz <j.e.diaz@icloud.com>
2021-06-27 23:19:28 -0700

    set up file structure and the db

---
+++ db/schema.sql
@@ -0,0 +1,10 @@
+DROP DATABASE IF EXISTS employee_trackerDB;
+CREATE DATABASE employee_trackerDB;
+
+USE employee_trackerDB;
+
+CREATE TABLE department(
+    id INT PRIMARY KEY,
+    name VARCHAR(30) 
+):
+
