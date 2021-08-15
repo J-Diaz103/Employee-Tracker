27dd21ce0a992a33c45198471076aaa139fe4a63
J Diaz <j.e.diaz@icloud.com>
2021-08-14 19:42:45 -0700

    merged files

--- db/schema.sql
+++ db/schema.sql
@@ -1,33 +1,30 @@
-DROP DATABASE IF EXISTS employee_trackerDB;
-CREATE DATABASE employee_trackerDB;
+DROP DATABASE IF EXISTS employees;
+CREATE DATABASE employees;
 
-USE employee_trackerDB;
+USE employees;
 
-CREATE TABLE department(
-    id INT USIGNED AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30) NOT NULL
-    PRIMARY KEY (id)
-):
-
-CREATE TABLE role(
-    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(30) NOT NULL,
-    salary DECIMAL default 0,
-    department_id INT UNSIGNED NOT NULL,
-    INDEX dept_ind (department_id),
-    FOREIGN KEY ( department_id),
-    CONSTRAINT fk_department REFERENCES department(id) ON DELETE CASCADE 
+CREATE TABLE department (
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) UNIQUE NOT NULL
 );
 
-CREATE TABLE employee(
-    id INT PRIMARY KEY,
-    first_name VARCHAR(30) INT NOT NULL,
-    last_name VARCHAR(30) INT NOT NULL,
-    role_id INT USIGNED NOT NULL, 
-    INDEX role_ind(role_id),
-    FOREIGN KEY (role_id),
-    REFERENCES role(id) ON DELETE CASCADE,
-    manager_id INT UNSIGNED ,
-    INDEX man_ind (manager_id),
-    FOREIGN KEY(manager_id) REFERENCES employee(id) ON DELETE SET NULL
+CREATE TABLE role (
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) UNIQUE NOT NULL,
+  salary DECIMAL UNSIGNED NOT NULL,
+  department_id INT UNSIGNED NOT NULL,
+  INDEX dep_ind (department_id),
+  CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
+);
+
+CREATE TABLE employee (
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT UNSIGNED NOT NULL,
+  INDEX role_ind (role_id),
+  CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
+  manager_id INT UNSIGNED,
+  INDEX man_ind (manager_id),
+  CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 );
---
+++ index.js
@@ -0,0 +1,264 @@
+// dependencies
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+const util = require("util");
+require("console.table");
+
+// create the connection for the sql database
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+// connect to the mysql server and sql database
+connection.connect((err) => {
+  if (err) throw err;
+  start();
+});
+
+// promisify all connection queries
+connection.query = util.promisify(connection.query);
+
+// starts the CLI
+const start = () => {
+  inquirer
+    .prompt({
+      name: "action",
+      type: "list",
+      message: "What would you like to do?",
+      choices: [
+        "View All Departments",
+        "View All Roles",
+        "View All Employees",
+        "Add Department",
+        "Add Role",
+        "Add Employee",
+        "Update Employee Role",
+        "Exit",
+      ],
+    })
+    .then((answer) => {
+      switch (answer.action) {
+        case "View All Departments":
+          viewAllDepartments();
+          break;
+        case "View All Roles":
+          viewAllRoles();
+          break;
+        case "View All Employees":
+          viewAllEmployees();
+          break;
+        case "Add Department":
+          addDepartment();
+          break;
+        case "Add Role":
+          addRole();
+          break;
+        case "Add Employee":
+          addEmployee();
+          break;
+        case "Update Employee Role":
+          updateEmployeeRole();
+          break;
+        case "Exit":
+          connection.end();
+          break;
+      }
+    });
+};
+
+// define view functions
+const viewAllDepartments = () => {
+  connection.query(
+    "SELECT department.id, department.name FROM department",
+    (err, res) => {
+      if (err) throw err;
+      console.table(res);
+      start();
+    }
+  );
+};
+
+const viewAllRoles = () => {
+  connection.query(
+    "SELECT role.id, role.title, role.salary, department.name AS department FROM role LEFT JOIN department ON role.department_id = department.id",
+    (err, res) => {
+      if (err) throw err;
+      console.table(res);
+      start();
+    }
+  );
+};
+
+const viewAllEmployees = () => {
+  connection.query(
+    `SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee AS manager RIGHT JOIN employee ON employee.manager_id = manager.id LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id`,
+    (err, res) => {
+      if (err) throw err;
+      console.table(res);
+      start();
+    }
+  );
+};
+
+// define add functions
+const addDepartment = () => {
+  inquirer
+    .prompt({
+      name: "name",
+      type: "input",
+      message: "What is the name of the department?",
+    })
+    .then((answer) => {
+      connection.query(
+        "INSERT INTO department SET ?",
+        {
+          name: answer.name,
+        },
+        (err) => {
+          if (err) throw err;
+          console.log(`Added ${answer.name} department to the database`);
+          start();
+        }
+      );
+    });
+};
+
+const addRole = async () => {
+  inquirer
+    .prompt([
+      {
+        name: "title",
+        type: "input",
+        message: "What is the title of the role?",
+      },
+      {
+        name: "salary",
+        type: "input",
+        message: "What is the salary of the role?",
+      },
+      {
+        name: "departmentId",
+        type: "list",
+        message: "What department is the role in?",
+        choices: await departmentChoices(),
+      },
+    ])
+    .then((answer) => {
+      connection.query(
+        "INSERT INTO role SET ?",
+        {
+          title: answer.title,
+          salary: answer.salary,
+          department_id: answer.departmentId,
+        },
+        (err) => {
+          if (err) throw err;
+          console.log(`Added ${answer.title} role to the database`);
+          start();
+        }
+      );
+    });
+};
+
+const addEmployee = async () => {
+  inquirer
+    .prompt([
+      {
+        name: "firstName",
+        type: "input",
+        message: `What is the employee's first name?`,
+      },
+      {
+        name: "lastName",
+        type: "input",
+        message: `What is the employee's last name?`,
+      },
+      {
+        name: "roleId",
+        type: "list",
+        message: `What is the employee's role?`,
+        choices: await roleChoices(),
+      },
+      {
+        name: "managerId",
+        type: "list",
+        message: `Who is the employee's manager?`,
+        choices: await employeeChoices(),
+      },
+    ])
+    .then((answer) => {
+      connection.query(
+        "INSERT INTO employee SET ?",
+        {
+          first_name: answer.firstName,
+          last_name: answer.lastName,
+          role_id: answer.roleId,
+          manager_id: answer.managerId,
+        },
+        (err) => {
+          if (err) throw err;
+          console.log(
+            `Added ${answer.firstName} ${answer.lastName} to the database`
+          );
+          start();
+        }
+      );
+    });
+};
+
+// define update functions
+const updateEmployeeRole = async () => {
+  inquirer
+    .prompt([
+      {
+        name: "id",
+        type: "list",
+        message: `Which employee's role do you want to update?`,
+        choices: await employeeChoices(),
+      },
+      {
+        name: "roleId",
+        type: "list",
+        message: `What is the employee's new role?`,
+        choices: await roleChoices(),
+      },
+    ])
+    .then((answer) => {
+      connection.query(
+        "UPDATE employee SET ? WHERE ?",
+        [
+          {
+            role_id: answer.roleId,
+          },
+          {
+            id: answer.id,
+          },
+        ],
+        (err) => {
+          if (err) throw err;
+          console.log(`Updated employee's role`);
+          start();
+        }
+      );
+    });
+};
+
+// query functions that pull choices
+const departmentChoices = async () =>
+  await connection.query(
+    "SELECT department.name AS name, department.id AS value FROM department ORDER BY name"
+  );
+
+const roleChoices = async () =>
+  await connection.query(
+    "SELECT role.title AS name, role.id AS value FROM role ORDER by name"
+  );
+
+const employeeChoices = async () =>
+  await connection.query(
+    `SELECT CONCAT(employee.first_name, ' ', employee.last_name) AS name, employee.id AS value FROM employee ORDER BY name`
+  );
